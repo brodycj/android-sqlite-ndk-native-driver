75e9ea8cd80547e05151ead470dc84f8b8f71d01
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-13 10:08:04 +0800

    Delete jni directory

--- jni/Application.mk
+++
@@ -1,7 +0,0 @@
-# APP_ABI := all
-APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
-
-# Explicit setting ref:
-# - https://developer.android.com/ndk/guides/stable_apis
-# - https://developer.android.com/studio/releases/platforms
-APP_PLATFORM := android-22
